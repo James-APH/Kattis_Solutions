2489f808d1baf9168a42716dbf810afc1d0681d4
James Huston <jamzhuston@gmail.com>
2023-09-16 13:54:50 -0600

    created more kattis files

---
+++ ToiletSeat.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+/*
+ * When you leave, always leave the seat up
+ *
+ * When you leave, always leave the seat down
+ *
+ * When you leave, always leave the seat as you would like to find it
+ *
+ * Your task is to evaluate these different policies. 
+ * For a given sequence of people’s preferences, 
+ * you are supposed to calculate how many seat adjustments are made for each policy.
+ *
+ * Test Val:
+ * UUUDDUDU
+ *    U U U D D U D U
+ * 1: 0 0 0 2 0 1 2 1
+ * 2: 0 1 1 2 0 1 2 1
+ * 3: 0 
+ */
+
+int main() {
+  string positions;
+  cin >> positions;
+
+
+
+
+
+}
+
