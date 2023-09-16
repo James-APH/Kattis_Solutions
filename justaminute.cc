9e76b8f3197cd4aa67d02c22ae299d84289d5706
James Huston <jamzhuston@gmail.com>
2023-09-16 13:55:26 -0600

    Fixed justaminute to output string, when neccesary

--- justaminute.cc
+++ justaminute.cc
@@ -9,22 +9,25 @@ double getSLminute(int);
 
 
 int main() {
-    int obs;
-    cin >> obs;
-    double SL_min = getSLminute(obs);
+  int obs;
+  cin >> obs;
+  double SL_min = getSLminute(obs);
+  if(SL_min <= 1) {
+    cout << "measurement error" << endl;
+  } else {
     cout << setprecision(7) << fixed << SL_min << endl;
-
+  }
 }
 
 double getSLminute(int obs) {
-    double sec, min;
-    double sec_sum = 0, min_sum = 0;
-    for(int i = 0; i < obs; i++) {
-        cin >> min >> sec;
-        min_sum += min;
-        sec_sum += sec;
-    }
-    return sec_sum / (min_sum * 60);
+  double sec, min;
+  double sec_sum = 0, min_sum = 0;
+  for(int i = 0; i < obs; i++) {
+    cin >> min >> sec;
+    min_sum += min;
+    sec_sum += sec;
+  }
+  return sec_sum / (min_sum * 60);
 }
 
 
