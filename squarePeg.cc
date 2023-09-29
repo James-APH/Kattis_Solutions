5e54d8a0c5b8b494e92427c3b0fc50c2e3ce1ab0
James Huston <jamzhuston@gmail.com>
2023-09-29 14:53:49 -0600

    did a lil reformatting on squarePeg, make it more organized

--- squarePeg.cc
+++ squarePeg.cc
@@ -5,45 +5,29 @@
 
 using namespace std;
 
-// N circular plots
-// M circular houses
-// K square houses
-//
-// can only be one house on each plot
-//
-// houses can't touch boundaries of the plots
-//
-// figure out how many plots can be filled
-//
-// First line: N M K
-// Next line N1, N2, N3, N4, Nn
-// Next line M1, M2, M3, Mn
-// Next line K1, K2, Kn
-//
-// Limits: all var >= 1, <= 100
-// set used plots to zero
 
 void setVectors(int N, int M, int K, vector<double>& plots, vector<double>& houses) 
 {
   int plot_radius, side_length;
 
-
-  for(int i = 0; i < N; i++) {
+  for(int i = 0; i < N; i++) 
+  {
     cin >> plot_radius;
     plots.push_back(plot_radius);
   }
 
-  for(int i = 0; i < M; i++) {
+  for(int i = 0; i < M; i++) 
+  {
     cin >> plot_radius;
     houses.push_back(plot_radius);
   }
 
-  for(int i = 0; i < K; i++) {
+  for(int i = 0; i < K; i++) 
+  {
     cin >> side_length;
     double square_radius = (sqrt(pow(side_length, 2) + pow(side_length, 2))/2);
     houses.push_back(square_radius);
   }
-
 
   sort(plots.begin(), plots.end());
   sort(houses.begin(), houses.end());
@@ -65,10 +49,13 @@ int main() {
 
   cin >> N >> M >> K;
   
-  iters = (N <= M+K ? N: M+K);
   setVectors(N, M, K, plots, houses);
+  
 
-  for(int i = 0; i < iters; i++) {
+  iters = (N <= (M + K) ? N: (M + K));
+  
+  for(int i = 0; i < iters; i++) 
+  {
     plots_to_fill += (houses[i] < plots[i]);
   }
 
