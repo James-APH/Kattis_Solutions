a0a736cf5d609682bffd27372d6f0e2d21a651b0
James Huston <jamzhuston@gmail.com>
2023-10-03 12:36:04 -0600

    created avion.cc

---
+++ avion.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+
+bool contains(string strToFind, string str, int length) {
+  for(int i = 0; i < length; i++) {
+
+
+
+  }
+  return false;
+}
+
+
+
+
+
+int main() {
+
+
+
+}
