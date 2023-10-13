b41c0556b516f8e97f889fa3b4ca79c68b1c2bf6
James Huston <jamzhuston@gmail.com>
2023-10-13 16:24:17 -0600

    finished coding FizzBuzz.cc

---
+++ FizzBuzz.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void fizzBuzz(int x, int y, int n) {
+  if(n%x == 0 && n%y == 0)
+  {
+    cout << "FizzBuzz" << endl;
+  }
+  else if(n%x == 0)
+  {
+    cout << "Fizz" << endl;
+  }
+  else if(n%y == 0)
+  {
+    cout << "Buzz" << endl;
+  }
+  else
+    cout << n << endl;
+}
+
+int main () {
+  int x, y, n;
+  cin >> x >> y >> n;
+
+  for(int i = 1; i <= n; i++) {
+    fizzBuzz(x, y, i);
+  }
+}
