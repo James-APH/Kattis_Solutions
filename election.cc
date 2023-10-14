adb013b6a31ed8dadb4c90fb9febd235f37c274d
James Huston <jamzhuston@gmail.com>
2023-10-14 12:20:41 -0600

    created election file

---
+++ election.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+using namespace std;
+
+// only two candidates remain on the ballot
+// **N** voters, everyone votes for 1 of the 2 candidates
+// inorder to win a candidate > 1/2 of the votes.
+// A certain number M <= N of ballots have been counted
+// There are Vi votes for candidate i (v1 + v2 = M)
+// 
+// remaining votes have a 50% chance to go to your candidate
+// if probability of winning exceeds a certain threshold W
+// victory is yours
+
+
+int main() {
+  int t;
+  cin >> t;
+  for(int i = 0; i < t; i++) {
+    int n, w, v1, v2, m;
+    cin >> n >> w >> v1 >> v2 >> m;
+    
+
+
+  }
+
+}
