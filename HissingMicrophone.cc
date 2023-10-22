3c953ee0eee3f8afa530f5962f40377237a33ea7
James Huston <jamzhuston@gmail.com>
2023-10-22 14:36:30 -0600

    finish HissingMicrophone

---
+++ HissingMicrophone.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+  string str;
+  cin >> str;
+  int length = str.length()-1;
+  bool hiss = false;
+  for(int i = 0; i < length; i++) {
+    if (str[i] == 's' && str[i+1] == 's') {
+      hiss = true;
+    }
+  }
+  cout << (hiss ? "hiss" : "no hiss") << endl;
+}
