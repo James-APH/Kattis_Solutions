10ad234cdc13f57f11b691a1a88a6c4e0c5abbd5
James Huston <jamzhuston@gmail.com>
2023-12-26 17:47:31 -0700

    Added file

---
+++ OutOfSorts.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ different.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+typedef long long ll;
+
+int main() {
+    ll num1, num2;
+    while (!stop && std::cin >> num1 >> num2) {
+        std::cout << abs(num1 - num2) << std::endl;
+    }
+}
