9f6271ab2df9fb615e8c47c6d71d9ab85652b3a6
James Huston <jamzhuston@gmail.com>
2024-01-15 13:30:32 -0700

    Feat: created kikiboba

---
+++ kikiboba.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+
+int main () {
+  std::string str;
+  std::cin >> str;
+  int bCount = 0, kCount = 0;
+
+  for (int i = 0; i < str.length(); i++) {
+    if (str[i] == 'b') {
+      bCount++;
+    }
+    if (str[i] == 'k') {
+      kCount++;
+    }
+  }
+  if (bCount > kCount) {
+    std::cout << "boba" << std::endl;
+  } else if (kCount > bCount) {
+    std::cout << "kiki" << std::endl;
+  } else if (kCount == bCount && kCount != 0) {
+    std::cout << "boki" << std::endl;
+  } else {
+    std::cout << "none" << std::endl;
+  }
+}
