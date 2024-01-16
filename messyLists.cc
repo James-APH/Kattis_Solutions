0430994ced89e21d25bb3e8b1ee9dc70a7a477ac
James Huston <jamzhuston@gmail.com>
2024-01-16 00:46:57 -0700

    Feat: created messyLists

---
+++ messyLists.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main () {
+  std::vector<int> vect;
+  std::vector<int> vectClone;
+
+  int length;
+  std::cin >> length;
+  for (int i = 0; i < length; i++) {
+    int element;
+    std::cin >> element;
+    vect.push_back(element);
+  }
+  vectClone = vect;
+
+  sort(begin(vect), end(vect));
+
+  int count = 0;
+  for (int i = 0; i < length; i++) {
+    if (vect[i] != vectClone[i])
+      count++;
+  }
+  std::cout << count << std::endl;
+}
