42266856113072a6514dda0a2a939067b1b8401e
James Huston <jamzhuston@gmail.com>
2024-01-18 13:20:21 -0700

    Feat: created and finished hayPoints

---
+++ hayPoints.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+
+int main() {
+  int m, n;
+  std::unordered_map<std::string, int> jobs;
+  std::unordered_map<std::string, int>::iterator it;
+  std::cin >> m >> n;
+
+  for (int i = 0; i < m; i++) {
+    std::string job;
+    int price;
+    std::cin >> job >> price;
+    jobs.insert({job, price});
+  }
+
+  
+  for (int i = 0; i < n; i++) {
+    int cost = 0;
+    std::string keyWord = "";
+    while (keyWord != ".") {
+      std::cin >> keyWord;
+      it = jobs.find(keyWord);
+      if (it != end(jobs)) {
+        cost += jobs[keyWord];
+      }
+    }
+    std::cout << cost << std::endl;
+  }
+}
