1dc763dfa7a438e0272afa5dbee1dddab4b22e47
James Huston <jamzhuston@gmail.com>
2024-02-02 16:48:41 -0700

    Feat: created and finished

---
+++ modulo.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unordered_set>
+
+int main() {
+  std::unordered_set<int> list;
+
+  for (int i = 0; i < 10; i++) {
+    int n;
+    std::cin >> n;
+    list.insert(n%42);
+  }
+  std::cout << list.size() << std::endl;
+}
