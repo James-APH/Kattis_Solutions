f15e1124c8a397710921c50ff3fd8d6fc16ac87f
James Huston <jamzhuston@gmail.com>
2024-02-05 22:21:27 -0700

    Created and finished magic trick, never added

---
+++ magictrick.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+int main () {
+    std::string input;
+    std::cin >> input;
+    std::sort(begin(input), end(input));
+    for (int i = 0; i < input.length()-1; i++) {
+        if (input[i] == input[i+1]) {
+            std::cout << 0 << std::endl;
+            return 0;
+        }
+    }
+    std::cout << 1 << std::endl;
+    return 0;
+}
