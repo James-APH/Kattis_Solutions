c6fc60b4ef03e727babaa2d92f2996bdd6ee3df5
James Huston <jamzhuston@gmail.com>
2024-05-01 15:44:23 -0600

    created and coded fiftyshades.py

---
+++ fiftyshades.py
@@ -0,0 +1,12 @@
+n = int(input())
+count = int(0)
+
+for x in range(n):
+    str = input().lower()
+    if "pink" in str or "rose" in str:
+        count += 1
+
+if (count == 0):
+    print("I must watch Star Wars with my daughter")
+else:
+    print(count)
