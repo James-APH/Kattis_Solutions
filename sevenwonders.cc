172d8f94d4872912f49404de3df77d599308559b
James Huston <jamzhuston@gmail.com>
2024-05-15 17:15:26 -0600

    created and coded sevenwonders.cc

---
+++ sevenwonders.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <cmath>
+
+
+
+int main () {
+  char chars[] = {'T', 'C', 'G'};
+  std::unordered_map<char, int> cardNumbers;
+  std::string input;
+  int sum = 0;
+  int min = 51;
+  std::cin >> input;
+  for (auto& str : input) {
+    cardNumbers[str] += 1;
+  }
+  //
+  for (auto& c : chars) {
+    sum += std::pow(cardNumbers[c],2);
+  }
+
+  for (auto& c : chars) {
+    if (cardNumbers[c] < min)
+      min = cardNumbers[c];
+  }
+
+  sum += min*7;
+
+
+  std::cout << sum << std::endl;
+}
